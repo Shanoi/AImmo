cab3bcd3450a1566d14646649e8e8c90b18d49f6
emre01100 <emre.eraslan@hotmail.fr>
2016-04-07 12:13:09 +0200

    Partie modifier
    Ajout de l'option modifier

---
+++ ClientWPF/modify_view.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour modify_view.xaml
+    /// </summary>
+    public partial class modify_view : Window
+    {
+        public modify_view()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_valider_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
